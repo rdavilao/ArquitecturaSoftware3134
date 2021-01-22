443de07ab6bfb5f8ec5ae75496d3da2ed941c68c
rdavilao <rdavila@espe.edu.ec>
2021-01-22 17:17:11 -0500

    Eliminacion de carpeta de deber

--- 1-PrimerParcial/Deberes/Banco-Banquito2/src/database/mongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const URI = 'mongodb://localhost/corebancario';
-
-mongoose.connect(URI)
-    .then(db => console.log("Base conectada"))
-    .catch(err => console.log(err));
-
-module.exports = mongoose;
